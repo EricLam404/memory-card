251df1b21b5e74dc3025cb6ec107c3826372a229
EricLam404 <lameric9888@gmail.com>
2023-01-01 18:38:05 -0500

    Added headers

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [score, setScore] = useState(0);
+  const [bestScore, setBestScore] = useState(0);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id="header">
+        <div id= "header-left">
+          <div id="header-title">
+            <h1>Memory Game</h1>
+          </div>
+          <div>Get points by clicking on an a new image! The game ends if you clicked on an image you previously  clicked</div>
+        </div>
+        <div id= "header-right">
+          <div>Score: {score}</div>
+          <div>Best Score: {bestScore}</div>
+        </div>
+      </div>
     </div>
   );
 }
