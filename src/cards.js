5d5a93c40affaff82486c0498e3ae22c022bbcb5
EricLam404 <lameric9888@gmail.com>
2023-01-03 20:52:57 -0500

    Added cards

--- src/cards.js
+++ src/cards.js
@@ -1,9 +1,12 @@
-import { useState } from 'react';
+import React, { useState, useEffect } from "react";
 
 function Cards(props) {
-    const [clickedCards, setClickedCards] = useState([]);
-    const cards = createCards(10);
+    const [cards, setCards] = useState(createCards(10));
     
+    useEffect(() => {
+        shuffle();
+    }, []);
+      
 
     function createCards(length){
         let arr = [];
@@ -15,11 +18,23 @@ function Cards(props) {
         }
         return arr;
     }
+    function shuffle() {
+        const shuffledCards = [...cards];
+        for (let i = shuffledCards.length - 1; i > 0; i--) {
+          const j = Math.floor(Math.random() * (i + 1));
+          [shuffledCards[i], shuffledCards[j]] = [shuffledCards[j], shuffledCards[i]];
+        }
+        setCards(shuffledCards);
+    }
+      
+    function handleClick(e){
+
+    }
     return (
         <div id="cards">
             {cards.map((card) => {
                 return(
-                    <div className="card" id={card.id} key={card.id}>
+                    <div className="card" id={card.id} key={card.id} onClick={(e) => handleClick(e)}>
                         {card.id}
                     </div>
                 );
